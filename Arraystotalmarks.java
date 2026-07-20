debc49b0a236c74104280a1c6bed77f2b0d962df
Devadharshini D <devadharshiniduraisamy3@gmail.com>
2026-07-20 14:41:14 +0530

    Create Arraystotalmarks.java

---
+++ Arraystotalmarks.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int[] Totalmarks={470,480,390,460,350,434};
+        String[]name={"Chandru","Dharshini","Vel","Jenifer","Anu","Deva"};
+        for (int i=0;i<Totalmarks.length;i++){
+            System.out.println(name[i]+"="+Totalmarks[i]);
+        }
+    }
+    }
